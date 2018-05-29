c9217581e09fc4da6d65a6b500ac3d45ebb15dc1
Mohamed Harmanani <mohamed.harmanani@mail.utoronto.ca>
2018-05-28 21:10:58 -0400

    started work on AI functions

---
+++ src/AIStrategy.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class AIStrategy {
+	
+	private String player;
+	private String self;
+	
+	AIStrategy() {
+		player = "X";
+		self = "O";
+	}
+	
+	public int minimax(BoardModel board) {
+		
+		if (board.getWinner() == self) {
+			return 1;
+		}
+		
+		else if (board.getWinner() == player) {
+			return -1;
+		}
+		
+		else {
+			ArrayList<Symbol> empty = board.emptyCells();
+			if (empty.size() == 0)
+				return 0;
+			
+		}
+		return 0;
+		
+	}
+	
+}
