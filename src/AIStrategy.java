c4d893dde857f557866ec1fd9ebde80c52d6dcaf
Mohamed Harmanani <mohamed.harmanani@mail.utoronto.ca>
2018-05-28 23:42:55 -0400

    added gitignore

--- src/AIStrategy.java
+++ src/AIStrategy.java
@@ -4,13 +4,15 @@ public class AIStrategy {
 	
 	private String player;
 	private String self;
+	private int reward;
 	
 	AIStrategy() {
 		player = "X";
 		self = "O";
+		reward = 0;
 	}
 	
-	public int minimax(BoardModel board) {
+	public int minimax(String current, BoardModel board) {
 		
 		if (board.getWinner() == self) {
 			return 1;
@@ -22,8 +24,19 @@ public class AIStrategy {
 		
 		else {
 			ArrayList<Symbol> empty = board.emptyCells();
+			ArrayList<int[]> locations = new ArrayList<int[]>();
 			if (empty.size() == 0)
 				return 0;
+			for (Symbol sym : empty) {
+				locations.add(sym.getPos());
+				board.makeMove(current, sym.getPos()[0], sym.getPos()[1]);
+				if (current == self) {
+					reward = minimax(self, board);
+				} else {
+					reward = minimax(player, board);
+				}
+				board.makeMove("_", sym.getPos()[0], sym.getPos()[1]);				
+			}
 		}
 		return 0;
 	}
